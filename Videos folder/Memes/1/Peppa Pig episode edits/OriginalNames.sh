6c81bfd0cb2fe1fe2eb6e02fa1b6a20ca4e53e68
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-19 16:35:05 -0800

    OriginalNames.sh
    A file that displays the original name of directories before the FUSEBLK compatibiility update.

---
+++ Videos folder/Memes/1/Peppa Pig episode edits/OriginalNames.sh
@@ -0,0 +1,5 @@
+'(No audio) MLG Peppa Pig| Peppa'\''s Air Horn!.mp4'
+'(No audio) Peppa Pig Plays Minecraft.mp4'
+'(No video) MLG Peppa Pig| Peppa'\''s Air Horn!.webm'
+'(No video) Peppa Pig Plays Minecraft.webm'
+
